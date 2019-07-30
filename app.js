22af41f1cdda425355d0a19cd7f8f82a9e30dca5
bprovansal <bprovansal@gmail.com>
2019-07-30 18:34:12 -0400

    app listener

--- app.js
+++ app.js
@@ -85,6 +85,9 @@ app.get("/api/displayFavorites", function(req, res) {
   });//connection
 });//route
 
-app.listen("8081", "0.0.0.0", function() {
+/*app.listen("8081", "0.0.0.0", function() {
   console.log("Express server is running...");
+});*/
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("Express Server is running.");
 });
